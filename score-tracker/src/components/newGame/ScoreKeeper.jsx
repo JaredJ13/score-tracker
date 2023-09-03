580a5ae628a4a7081db6bac7cd4060ec7d21e913
Jared Jahnke <77257554+JaredJ13@users.noreply.github.com>
2023-09-02 20:06:10 -0600

    score history, add teams/score input enhancements

--- score-tracker/src/components/newGame/ScoreKeeper.jsx
+++ score-tracker/src/components/newGame/ScoreKeeper.jsx
@@ -11,6 +11,11 @@ import {
   DialogTitle,
   DialogContent,
   DialogActions,
+  Divider,
+  Chip,
+  Paper,
+  Card,
+  CardContent,
 } from "@mui/material";
 import { Chart } from "react-google-charts";
 
@@ -19,13 +24,15 @@ import AddIcon from "@mui/icons-material/Add";
 
 export default function ScoreKeeper() {
   const [teams, setTeams] = useState([]);
-  const [scores, setScores] = useState([]);
-  const [seed, setSeed] = useState(0);
-
-  const [chartData, setChartData] = useState([["Teams", "Scores"]]);
+  const [scoreHistory, setScoreHistory] = useState([]);
+  const [currentRound, setCurrentRound] = useState(1);
+  const [chartData, setChartData] = useState([["Team", "Score"]]);
+  const [startMatchButtonDisabled, setStartMatchButtonDisabled] =
+    useState(true);
 
   // input state
-  const [addTeamName, setAddTeamName] = useState("");
+  const [addTeam1Name, setAddTeam1Name] = useState("");
+  const [addTeam2Name, setAddTeam2Name] = useState("");
   const [scoreInput, setScoreInput] = useState(0);
 
   // modal state
@@ -46,116 +53,266 @@ export default function ScoreKeeper() {
   };
 
   const handleAddNewTeam = () => {
-    let newTeamName = addTeamName;
-    setChartData([...chartData, [newTeamName, 0]]);
-    setTeams([...teams, addTeamName]);
-    setSeed(seed + 1);
+    setChartData([...chartData, [addTeam1Name, 0], [addTeam2Name, 0]]);
+    setTeams([...teams, addTeam1Name, addTeam2Name]);
+    handleAddTeamModal();
   };
 
+  useEffect(() => {
+    // on component render force user to enter teams
+    // open modal
+    handleAddTeamModal();
+  }, []);
+
   const handleAddPoints = (team, index) => {
-    let chartDataArray = chartData;
-    let scoreIndex = chartDataArray.findIndex(
-      (element) => element[0] === teams[index]
-    );
-    chartDataArray.splice(scoreIndex, 1, [team, Number(scoreInput)]);
-    setChartData(chartDataArray);
-    setScoreInput(0);
+    // add score to score history if score hasn't been added yet for the team this round
+    let teamValid = true;
+    scoreHistory.map((score) => {
+      if (score.team === team && score.round === currentRound) {
+        teamValid = false;
+      }
+    });
+    if (teamValid) {
+      let scoreItemNum = scoreHistory.length;
+      let newScoreTotal = 0;
+      // calc new score total
+      if (scoreHistory.length > 0 && scoreHistory.length !== undefined) {
+        scoreHistory.map((score) => {
+          if (score.team === team) {
+            newScoreTotal += score.scoreUpdate;
+          }
+        });
+        newScoreTotal += parseInt(scoreInput);
+      } else {
+        newScoreTotal = parseInt(scoreInput);
+      }
+      setScoreHistory([
+        ...scoreHistory,
+        {
+          team: team,
+          scoreUpdate: parseInt(scoreInput),
+          scoreEntry: scoreItemNum,
+          teamScoreTotal: newScoreTotal,
+          round: currentRound,
+        },
+      ]);
+      // input current score into chart data
+      let chartDataArray = chartData;
+      let scoreIndex = chartDataArray.findIndex(
+        (element) => element[0] === teams[index]
+      );
+      chartDataArray.splice(scoreIndex, 1, [team, newScoreTotal]);
+      setChartData(chartDataArray);
+      // set score input state again to update google chart
+      setScoreInput(0);
+    } else {
+      console.log("Team already added score this round");
+    }
   };
+
+  useEffect(() => {
+    // Check if all teams have scored once this round
+    let map = new Map();
+    for (let i = 0; i < scoreHistory.length; i++) {
+      if (scoreHistory[i].round === currentRound) {
+        // add team and score to map
+        map.set(scoreHistory[i].team, scoreHistory[i].scoreUpdate);
+        // check if all teams participating are in map
+        if (map.size === teams.length) {
+          return setCurrentRound(currentRound + 1);
+        }
+      }
+    }
+  }, [scoreHistory]);
+
+  useEffect(() => {
+    // check if both team names have input
+    if (addTeam1Name.length >= 2 && addTeam2Name.length >= 2) {
+      setStartMatchButtonDisabled(false);
+    } else {
+      setStartMatchButtonDisabled(true);
+    }
+  }, [addTeam1Name, addTeam2Name]);
 
   return (
     <>
       <Container>
         {teams.length >= 1 && teams.length !== undefined ? (
-          <Chart
-            options={options}
-            chartType="ColumnChart"
-            width="100%"
-            height="300px"
-            data={chartData}
-            key={seed}
-          />
-        ) : (
-          <Grid item>
-            <Typography variant="h5" color="secondary" align="center" my={10}>
-              To start tracking your matche{"'"}s scores, add your teams below.
-            </Typography>
-          </Grid>
-        )}
-
-        <Grid container justifyContent="center" rowSpacing={2}>
-          <Grid item>
-            <Button
-              variant="text"
-              color="success"
-              size="small"
-              onClick={handleAddTeamModal}
-            >
-              Add Team +
-            </Button>
-          </Grid>
-          <Dialog open={addTeamModalOpen} onClose={handleAddTeamModal}>
-            <DialogTitle>Add a New Team</DialogTitle>
-            <DialogContent>
-              <TextField
-                autoFocus
-                margin="dense"
-                id="new-team-name"
-                label="Team Name"
-                type="text"
-                fullWidth
-                variant="standard"
-                onChange={(e) => setAddTeamName(e.target.value)}
-              />
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={handleAddTeamModal}>Cancel</Button>
-              <Button onClick={handleAddNewTeam}>Add Team</Button>
-            </DialogActions>
-          </Dialog>
-
-          {teams.length >= 1 && teams.length !== undefined ? (
-            teams.map((team, index) => {
-              return (
-                <Grid
-                  item
-                  container
-                  xs={12}
-                  justifyContent="center"
-                  alignItems="center"
-                >
-                  <Grid item xs={6}>
-                    <TextField
-                      id="outlined-basic"
-                      label={team}
-                      defaultValue={0}
-                      variant="outlined"
-                      fullWidth
-                      type="number"
-                      onChange={(e) => setScoreInput(e.target.value)}
+          <>
+            <Chart
+              options={options}
+              chartType="ColumnChart"
+              width="100%"
+              height="300px"
+              data={chartData}
+            />
+            <Grid container justifyContent="space-around">
+              <Grid item>
+                <Chip label={`Round: ${currentRound}`} color="primary" />
+              </Grid>
+              {teams.map((team) => {
+                let scoreHistoryIndex = scoreHistory.findIndex(
+                  (x) => x.team === team && x.round === currentRound
+                );
+                if (scoreHistoryIndex === -1) {
+                  scoreHistoryIndex = scoreHistory.findIndex(
+                    (x) => x.team === team && x.round === currentRound - 1
+                  );
+                }
+                return (
+                  <Grid item>
+                    <Chip
+                      label={
+                        scoreHistoryIndex !== -1
+                          ? `${team}'s Score: ${scoreHistory[scoreHistoryIndex].teamScoreTotal}`
+                          : `${team}'s Score: 0`
+                      }
+                      color="default"
                     />
                   </Grid>
-                  <Grid item>
-                    <IconButton
-                      aria-label="add-score"
-                      onClick={() => handleAddPoints(team, index)}
-                    >
-                      <AddIcon />
-                    </IconButton>
-                  </Grid>
-                </Grid>
-              );
-            })
-          ) : (
-            <Grid
-              item
-              container
-              xs={12}
-              justifyContent="center"
-              alignItems="center"
-            >
-              <Typography variant="body1">No Teams Added</Typography>
+                );
+              })}
             </Grid>
-          )}
+          </>
+        ) : (
+          <Grid item></Grid>
+        )}
+        <Paper sx={{ my: 4, pb: 4 }} square>
+          <Grid container justifyContent="center" rowSpacing={2}>
+            {teams.length === 0 || teams.length === undefined ? (
+              <Grid item>
+                <Button
+                  variant="text"
+                  color="success"
+                  size="small"
+                  onClick={handleAddTeamModal}
+                >
+                  Add Match Teams
+                </Button>
+              </Grid>
+            ) : (
+              ""
+            )}
+            <Dialog open={addTeamModalOpen} onClose={handleAddTeamModal}>
+              <DialogTitle>Input the Match{"'"}s Team Names</DialogTitle>
+              <DialogContent>
+                <TextField
+                  autoFocus
+                  margin="dense"
+                  id="new-team-name"
+                  label="Team Name"
+                  type="text"
+                  fullWidth
+                  variant="standard"
+                  onChange={(e) => setAddTeam1Name(e.target.value)}
+                />
+                <TextField
+                  autoFocus
+                  margin="dense"
+                  id="new-team-name"
+                  label="Team Name"
+                  type="text"
+                  fullWidth
+                  variant="standard"
+                  onChange={(e) => setAddTeam2Name(e.target.value)}
+                />
+              </DialogContent>
+              <DialogActions>
+                <Button
+                  disabled={startMatchButtonDisabled}
+                  onClick={handleAddNewTeam}
+                >
+                  Start Match
+                </Button>
+              </DialogActions>
+            </Dialog>
+
+            {teams.length >= 1 && teams.length !== undefined ? (
+              teams.map((team, index) => {
+                return (
+                  <Grid
+                    item
+                    container
+                    xs={12}
+                    justifyContent="center"
+                    alignItems="center"
+                    key={index}
+                  >
+                    <Grid item xs={6}>
+                      <TextField
+                        id="outlined-basic"
+                        label={team}
+                        defaultValue={0}
+                        variant="outlined"
+                        fullWidth
+                        type="number"
+                        onChange={(e) => setScoreInput(e.target.value)}
+                      />
+                    </Grid>
+                    <Grid item>
+                      <IconButton
+                        aria-label="add-score"
+                        onClick={() => handleAddPoints(team, index)}
+                      >
+                        <AddIcon />
+                      </IconButton>
+                    </Grid>
+                  </Grid>
+                );
+              })
+            ) : (
+              <Grid
+                item
+                container
+                xs={12}
+                justifyContent="center"
+                alignItems="center"
+              >
+                <Typography variant="body1">No Teams Added</Typography>
+              </Grid>
+            )}
+          </Grid>
+        </Paper>
+        <Divider>Score History</Divider>
+        <Typography align="center" variant="body2" sx={{ mb: 4 }}>
+          Score entries from Newest to Oldest
+        </Typography>
+        <Grid
+          container
+          justifyContent="center"
+          direction="column-reverse"
+          xs={12}
+          rowGap={2}
+        >
+          {scoreHistory.map((score) => {
+            return (
+              <Grid item>
+                <Card
+                  square
+                  sx={{
+                    backgroundColor:
+                      score.scoreUpdate > 0
+                        ? "#a4f8e0"
+                        : score.scoreUpdate === 0
+                        ? "#9eeffc"
+                        : "#fa9494",
+                  }}
+                >
+                  <CardContent>
+                    <Typography variant="h6" color="text.secondary">
+                      Round: {score.round} Team: {score.team}
+                    </Typography>
+                    <Typography variant="h5">
+                      Points added: {score.scoreUpdate}
+                    </Typography>
+                    <Typography variant="h6">
+                      Total Team Score: {score.teamScoreTotal}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            );
+          })}
         </Grid>
       </Container>
     </>
